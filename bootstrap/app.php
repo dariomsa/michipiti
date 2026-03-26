7ba66a1baadee8cff4e541d376bfa2cdc12e1924
root <root@dev.milhojas.is>
2026-03-25 22:33:25 -0500

    estilos visuales11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Http\Exceptions\PostTooLargeException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +18,18 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (PostTooLargeException $e, Request $request) {
+            $message = 'El archivo es demasiado grande. El limite actual es 30 MB por solicitud.';
+
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'message' => $message,
+                ], 413);
+            }
+
+            return redirect()
+                ->back()
+                ->withInput($request->except([]))
+                ->with('error', $message);
+        });
     })->create();
