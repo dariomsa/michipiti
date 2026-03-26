e02a2d0c6e806d25e5a1a26983674915f8f293c4
root <root@dev.milhojas.is>
2026-03-26 09:56:18 -0500

    estilos visuales20

--- app/Services/Carrusel/CarruselSlackNotifier.php
+++ app/Services/Carrusel/CarruselSlackNotifier.php
@@ -6,6 +6,7 @@ use App\Models\Producto;
 use App\Models\User;
 use App\Services\Slack\SlackNotificationService;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class CarruselSlackNotifier
 {
@@ -65,9 +66,25 @@ class CarruselSlackNotifier
             return;
         }
 
-        User::query()
+        $users = User::query()
             ->whereIn('id', $ids)
-            ->each(function (User $user) use ($texto): void {
+            ->get(['id', 'name', 'email', 'email_slack', 'slack_user_id']);
+
+        Log::info('[SLACK DEBUG] destinatarios movimiento/carrusel', [
+            'producto_id' => $producto->id,
+            'titulo' => $producto->titulo,
+            'actor_user_id' => $actorUserId,
+            'destinatarios' => $users->map(fn (User $user) => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'email_slack' => $user->email_slack,
+                'slack_user_id' => $user->slack_user_id,
+            ])->values()->all(),
+            'mensaje' => $texto,
+        ]);
+
+        $users->each(function (User $user) use ($texto): void {
                 $this->slack->sendDM($user, $texto);
             });
     }
